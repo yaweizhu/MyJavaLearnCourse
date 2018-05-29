6db107f806ac625335af151e1e4b8cb11b3260b3
yaweizhu <yaweizhu@users.noreply.github.com>
2018-05-29 17:57:43 +0800

    day4_1

---
+++ Mybranch/Operators.java
@@ -0,0 +1,20 @@
+class Operators 
+{
+	public static void main(String[] args) 
+	{
+		 char c = 'A' ; 
+                     int num = 10 ; 
+                     switch(c) { 
+                         case 'B' : 
+                             num ++ ; 
+                         case 'A' : 
+                             num ++ ; 
+                         case 'Y' : 
+                             num ++ ; 
+                             break ; 
+                         default : 
+                             num -- ; 
+                     } 
+                     System.out.println(num) ; 
+	}
+}
